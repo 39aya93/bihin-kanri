3ab688466bf6008f2ac9882ffb46fafcbc78d7af
39aya93 <39aya93@gmail.com>
2026-09-09 20:19:50 +0900

    Update service worker to version 5 with fetch caching

--- sw.js
+++ sw.js
@@ -1,67 +1,42 @@
-const CACHE_NAME = "bihin-kanri-v1";
+const CACHE_NAME = 'bihin-kanri-v5';
 
 const FILES = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon.svg"
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.svg'
 ];
 
-self.addEventListener(
-  "install",
-  event => {
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(c => c.addAll(FILES))
+  );
+  self.skipWaiting();
+});
 
-    event.waitUntil(
-      caches.open(CACHE_NAME)
-        .then(cache =>
-          cache.addAll(FILES)
-        )
-    );
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(k => k !== CACHE_NAME)
+          .map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
 
-    self.skipWaiting();
-  }
-);
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
 
-
-self.addEventListener(
-  "activate",
-  event => {
-
-    event.waitUntil(
-      caches.keys()
-        .then(keys =>
-          Promise.all(
-            keys
-              .filter(
-                key =>
-                  key !== CACHE_NAME
-              )
-              .map(
-                key =>
-                  caches.delete(key)
-              )
-          )
-        )
-    );
-
-    self.clients.claim();
-  }
-);
-
-
-self.addEventListener(
-  "fetch",
-  event => {
-
-    event.respondWith(
-
-      caches.match(event.request)
-        .then(cached => {
-
-          return cached ||
-            fetch(event.request);
-
-        })
-    );
-  }
-);
+  e.respondWith(
+    fetch(e.request)
+      .then(r => {
+        const copy = r.clone();
+        caches.open(CACHE_NAME).then(c => c.put(e.request, copy));
+        return r;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
